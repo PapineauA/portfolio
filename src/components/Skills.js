f923f5ceb8f8d71135c708003d864a384d5a11aa
PapineauA <papineautaidan@gmail.com>
2023-02-12 19:09:31 -0500

    Testing branch again

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,7 +12,8 @@ export default function Skills() {
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Skills
+            These are just some of the skills I have learned so far in this
+            field!
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
